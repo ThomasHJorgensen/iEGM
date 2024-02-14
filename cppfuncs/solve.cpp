44145f13adeeb6b032679076ec93af9be88afadf
Adam Hallengreen <90259162+AdamHallengreen@users.noreply.github.com>
2024-02-14 13:13:30 +0100

    fix(solve.cpp): Calculate EmargV over expectations

--- cppfuncs/solve.cpp
+++ cppfuncs/solve.cpp
@@ -41,10 +41,10 @@ EXPORT void compute_margEV(sol_struct* sol, par_struct* par){
         for (int iP=0; iP<par->num_power; iP++){
             for (int iL=0; iL<par->num_love; iL++){
                 int idx = index::couple(t,iP,iL,0,par);
-                double* Vw = &sol->Vw_start_as_couple[idx];
-                double* Vm = &sol->Vm_start_as_couple[idx];
-                double* margV = &sol->margV_start_as_couple[idx];
-                couple::calc_marginal_value_couple(t, iP, iL, Vw, Vm, margV, sol, par);
+                double* EVw = &sol->EVw_start_as_couple[idx];
+                double* EVm = &sol->EVm_start_as_couple[idx];
+                double* EmargV = &sol->EmargV_start_as_couple[idx];
+                couple::calc_marginal_value_couple(t, iP, iL, EVw, EVm, EmargV, sol, par);
             }
         }
     }
